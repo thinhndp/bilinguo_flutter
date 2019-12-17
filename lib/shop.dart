fae488a84ef98961481f9a2d2616d50fc30e333b
tligodsp <43437080+tligodsp@users.noreply.github.com>
2019-12-17 21:45:56 +0700

    Done shop

--- lib/shop.dart
+++ lib/shop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'mock-data.dart';
 
 class ShopScreen extends StatelessWidget {
   renderWidgetRepeatedLy(widgetList, repeatCount) {
@@ -62,13 +63,112 @@ class ShopScreen extends StatelessWidget {
       ),
     );
   }
+  
+  renderItemList(items) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: items.map<Widget>((item) => (
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+         child: Row(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: <Widget>[
+             Flexible(
+               flex: 1,
+               child: Container(
+                 padding: EdgeInsets.fromLTRB(10, 15, 0, 20),
+                 child: Image.asset('assets/' + item.imgUrl),
+               ),
+             ),
+             Flexible(
+               flex: 2,
+               child: Container(
+                 padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                 child: Column(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: <Widget>[
+                     Text(
+                         item.name,
+                         style: TextStyle(
+                             fontWeight: FontWeight.bold,
+                             fontSize: 18
+                         )
+                     ),
+                     SizedBox(height: 10),
+                     Text(
+                         item.description,
+                         style: TextStyle(
+                             fontWeight: FontWeight.w500,
+                             fontSize: 15,
+                             color: Colors.black54,
+                           height: 1.5,
+                         )
+                     ),
+                     SizedBox(height: 10),
+                     Row(
+                       children: <Widget>[
+                         Image.asset('assets/icons/lingot.png', width: 26),
+                         SizedBox(width: 3),
+                         Text(
+                             item.price.toString(),
+                             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Color(0xffff4b4b))
+                         )
+                       ],
+                     )
+                   ],
+                 ),
+               )
+             )
+           ],
+         ),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black12,
+              style: BorderStyle.none,
+//              width: 3,
+//              style: BorderStyle.solid
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            color: Colors.white,
+            boxShadow: [BoxShadow(
+              color: Colors.black26,
+              blurRadius: 4.0,
+            )],
+          ),
+        )
+      )).toList(),
+    );
+  }
+
+  renderItemGroup(itemGroup) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(itemGroup.name, style: TextStyle( fontSize: 24, fontWeight: FontWeight.bold )),
+          renderItemList(itemGroup.items)
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Column(
+      child: Stack(
         children: <Widget>[
+          Container(
+            child: ListView(
+              padding: EdgeInsets.fromLTRB(20, 55, 20, 30),
+              shrinkWrap: true,
+              children: <Widget>[
+                renderItemGroup(powerUps),
+                renderItemGroup(loremIpsum),
+              ],
+            ),
+          ),
           Stack(
             alignment: AlignmentDirectional.center,
             children: <Widget>[
